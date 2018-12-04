b55124d1b353acc3fb72e33c2aa18f6f682ea580
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-12-04 16:19:02 +0200

    yarn config

--- dev/ci/yarn-run.sh
+++ dev/ci/yarn-run.sh
@@ -5,6 +5,9 @@ set -e
 export npm_config_proxy=http://npm-proxy:8080
 export npm_config_https_proxy=http://npm-proxy:8080
 export npm_config_strict_ssl=false
+export yarn_proxy=http://npm-proxy:8080
+export yarn_https_proxy=http://npm-proxy:8080
+export yarn_strict_ssl=false
 
 echo "--- yarn"
 yarn --frozen-lockfile --network-timeout 60000
