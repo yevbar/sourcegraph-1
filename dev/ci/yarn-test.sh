4d4fabb47dd763178b8f5b357696d7e9d4dde82b
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-12-04 16:21:35 +0200

    yarn config list

--- dev/ci/yarn-test.sh
+++ dev/ci/yarn-test.sh
@@ -5,9 +5,11 @@ set -e
 export npm_config_proxy=http://npm-proxy:8080
 export npm_config_https_proxy=http://npm-proxy:8080
 export npm_config_strict_ssl=false
-export yarn_proxy=http://npm-proxy:8080
+export yarn_http_proxy=http://npm-proxy:8080
 export yarn_https_proxy=http://npm-proxy:8080
 export yarn_strict_ssl=false
+
+yarn config list
 
 echo "--- yarn in root"
 yarn --frozen-lockfile --network-timeout 60000
